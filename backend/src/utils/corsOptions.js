fdb45aea7df55382bb735ca2d47b6c6ba1e81865
Ravi <ravisharmabhimtal@gmail.com>
2025-07-24 00:37:52 +0530

    add frontend url

--- backend/src/utils/corsOptions.js
+++ backend/src/utils/corsOptions.js
@@ -1,7 +1,7 @@
 export const getCorsOptions = () => {
   const isProd = process.env.NODE_ENV === "production";
   return {
-    origin: isProd ? "https://your-production-domain.com" : "http://localhost:5173",
+    origin: isProd ? "https://munchiza.netlify.app" : "https://munchiza.netlify.app",
     credentials: true,
   };
 };
